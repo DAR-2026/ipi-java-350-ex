4b9f0406b65b391a531a68a6b380df1f59fbce70
C-S-Sylla <163921376+C-S-Sylla@users.noreply.github.com>
2026-03-25 10:06:33 +0000

    Update Employe.java

--- src/main/java/com/ipiecoles/java/java350/model/Employe.java
+++ src/main/java/com/ipiecoles/java/java350/model/Employe.java
@@ -62,17 +62,22 @@ public class Employe {
 public Integer getNbRtt(LocalDate d){
     int i1 = d.isLeapYear() ? 365 : 366; 
     int var = 104;
-    switch (LocalDate.of(d.getYear(),1,1).getDayOfWeek()){
-        case THURSDAY: 
-            if(d.isLeapYear()) var = var + 1; 
-            break;
-        case FRIDAY:
-            if(d.isLeapYear()) var = var + 2;
-            else var = var + 1;
-            break; // <--- IL MANQUAIT CE BREAK ICI
-        case SATURDAY:
-            var = var + 1;
-            break;
+        switch (LocalDate.of(d.getYear(),1,1).getDayOfWeek()){
+            case THURSDAY:
+                if(d.isLeapYear()) var = var + 1;
+                break; // Déjà présent, c'est bien.
+
+            case FRIDAY:
+                if(d.isLeapYear()) var = var + 2;
+                else var = var + 1;
+                break; // <--- AJOUTE CETTE LIGNE ICI pour corriger l'alerte !
+
+            case SATURDAY:
+                var = var + 1;
+                break;
+
+            default: // <--- AJOUTE ÇA AUSSI (Bonne pratique Sonar)
+                break;
         }
         int monInt = (int) Entreprise.joursFeries(d).stream().filter(localDate ->
                 localDate.getDayOfWeek().getValue() <= DayOfWeek.FRIDAY.getValue()).count();
